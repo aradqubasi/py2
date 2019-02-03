ba407975bae143944090075ca7b37233ddb85cad
olegsokolansky <oleg.sokolansky@gmail.com>
2019-02-03 10:22:34 +0200

    factory

--- application.py
+++ application.py
@@ -1,6 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+from flask import Flask
+
+
+def create_app() -> Flask:
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+    
+    return app
